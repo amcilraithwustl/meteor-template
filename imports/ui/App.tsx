f28265a5f0a168cce30ddcee6671e62bd0f4d7fc
amcilraithwustl <90517424+amcilraithwustl@users.noreply.github.com>
2022-06-10 15:50:45 -0500

    Updated dependencies

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,22 +1,9 @@
-import * as React from "react";
-import { Navigate, useLocation } from "react-router-dom";
-
-import { useTracker } from "meteor/react-meteor-data";
-import { TakePollPage } from "./TakePollPage";
-
-const ProtectedPage = ({ children }: { children: JSX.Element }) => {
-  const location = useLocation();
-  const userId = useTracker(() => Meteor.userId());
-  if (!userId) {
-    return <Navigate to="/login" state={{ from: location }} replace />;
-  }
-  return children;
-};
-
-const protect = (child: JSX.Element) => {
-  return <ProtectedPage>{child}</ProtectedPage>;
-};
+import React from "react";
 
 export const App = (): JSX.Element => {
-  return <TakePollPage />;
+  return (
+    <div>
+      <p>Hello World!</p>
+    </div>
+  );
 };
--- imports/ui/LogoutButton.tsx
+++
@@ -1,7 +0,0 @@
-import { Accounts } from "meteor/accounts-base";
-import { Button } from "@mui/material";
-import React from "react";
-
-export const LogoutButton = (): JSX.Element => {
-  return <Button onClick={() => Accounts.logout()}>Logout</Button>;
-};
